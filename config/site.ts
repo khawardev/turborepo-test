90318c797cb2d667b819b600e13c95a989cc8eab
Khawar Sultan <khawarsultan663@gmail.com>
2025-08-18 16:03:18 +0500

    Update global styles by removing unnecessary whitespace and refine site description for improved clarity and branding.

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
     name: "Brand Health Audit | Humanbrand AI",
     url: "http://localhost:3000",
-    description: "Get a free, comprehensive audit of your website's performance, SEO, and more.",
+    description: "Get Clarity. Start your free Website Brand Health",
 };
 
 export type SiteConfig = typeof siteConfig;
