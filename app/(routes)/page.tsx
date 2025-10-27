296bc71d933a67a2a225965ff8e7fcf998f0d3f8
Khawar Sultan <khawarsultan663@gmail.com>
2025-10-27 21:38:23 +0500

    Update markdown.css to change pre background color to var(--color-border) and remove border from blockquote for improved styling consistency. Refactor page.tsx by removing unused imports and unnecessary div wrapper to streamline the Home component.

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,19 +1,8 @@
 'use client'
 
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { ContainerMd } from "@/components/shared/containers";
-import { Plus } from "lucide-react";
-import BrandList from "@/components/brands/list/BrandList";
-import StaticBanner from "@/components/shared/staticBanner";
-import LaserFlow from "@/components/ui/react-bits/LaserFLow/LaserFlow";
 import HeroSection from "@/components/hero-section/hero-sction";
-
 export default function Home() {
   return (
-    <div>
       <HeroSection />
-      
-    </div>
   );
 }
